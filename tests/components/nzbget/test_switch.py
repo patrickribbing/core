f363bbf03172145998c98d01595c4c02d2160557
Chris Talkington <chris@talkingtontech.com>
2020-09-27 12:49:05 -0500

    Update test_switch.py

--- tests/components/nzbget/test_switch.py
+++ tests/components/nzbget/test_switch.py
@@ -5,32 +5,43 @@ from homeassistant.const import (
     SERVICE_TURN_OFF,
     SERVICE_TURN_ON,
     STATE_OFF,
+    STATE_ON,
 )
 
-from . import init_integration
+from . import MOCK_STATUS, init_integration
 
-from tests.async_mock import patch
+from tests.async_mock import MagicMock, patch
 
 
 async def test_download_switch(hass) -> None:
     """Test the creation and values of the download switch."""
-    entry = await init_integration(hass)
-
+    status = MagicMock(return_value=MOCK_STATUS.copy())
+    entry = await init_integration(hass, status=status)
     registry = await hass.helpers.entity_registry.async_get_registry()
 
-    entity_entry = registry.async_get(f"switch.nzbgettest_download")
+    entity_id = "switch.nzbgettest_download"
+    entity_entry = registry.async_get(entity_id)
     assert entity_entry
     assert entity_entry.unique_id == f"{entry.entry_id}_download"
 
-    state = hass.states.get("switch.nzbgettest_download")
+    state = hass.states.get(entity_id)
     assert state
     assert state.state == STATE_OFF
+
+    # test downloads paused
+    status.return_value["DownloadPaused"] = True
+
+    await hass.helpers.entity_component.async_update_entity(entity_id)
+    await hass.async_block_till_done()
+
+    state = hass.states.get(entity_id)
+    assert state
+    assert state.state == STATE_ON
 
 
 async def test_download_switch_services(hass) -> None:
     """Test download switch services."""
     await init_integration(hass)
-
     entity_id = "switch.nzbgettest_download"
 
     with patch("homeassistant.components.nzbget.coordinator.NZBGetAPI.pausedownload") as pause_mock:
